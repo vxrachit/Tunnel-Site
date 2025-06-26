a604267b47559195c15cd11510d3b8975d7cf5c6
vxrac <rachitv00@gmail.com>
2025-06-27 00:52:40 +0530

    updated config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://vxrachit-tunnel.pages.dev/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
   },
 });
